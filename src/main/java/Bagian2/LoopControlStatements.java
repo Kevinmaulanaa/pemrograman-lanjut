be9f7646772bdf8828809c28570d116870f48c03
brevv99 <key.scoup55@gmail.com>
2021-03-26 16:37:45 +0700

    Belajar loop control statement

---
+++ src/main/java/Bagian2/LoopControlStatements.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class LoopControlStatements {
+    public static void main(String[]args)
+    {
+     int x = 1;
+
+    do {
+
+    System.out.println(x);
+
+    x++;
+    }
+    while(x <= 5);
+
+    }
+}
