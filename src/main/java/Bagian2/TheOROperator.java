22beb0d2dcfa45ce8e3a10e5c86edfa6a9094b69
brevv99 <key.scoup55@gmail.com>
2021-03-26 15:22:15 +0700

    Belajar OR Operator

---
+++ src/main/java/Bagian2/TheOROperator.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class TheOROperator {
+    public static void main(String[]args)
+    {
+        //belajar materi or operator
+        
+        int a = 11; int b = 12;
+        int c = 40;
+        if (a > 100 || b > 3) {
+        System.out.println(a); 
+        } else {
+        System.out.println(c);
+        }
+    }
+}
