e202eb9db9f7a3ff31b65b383d722c7ce386393c
brevv99 <key.scoup55@gmail.com>
2021-03-26 14:56:28 +0700

    Belajar nested if

---
+++ src/main/java/Bagian2/Nested.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class Nested {
+    public static void main(String[]args)
+    {
+        int x = 37;
+        
+        if(x > 32)
+        {
+            System.out.println("It's Works!");
+        }
+    }
+}
