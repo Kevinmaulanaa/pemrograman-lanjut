c6b20b223365e50dd794e7cc3610fd37ce80dec4
brevv99 <key.scoup55@gmail.com>
2021-03-26 16:12:05 +0700

    Belajar while loops bagian 2

---
+++ src/main/java/Bagian2/whileLoops2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class whileLoops2 {
+    public static void main(String[]args)
+    {
+        int x = 0;
+        int y = 5;
+        while (x < y) {
+        System.out.println("Hello");
+        x++;	
+        }
+    }
+}
