744c762ad53e0ef6ea0bd49fd6aa92eea483a91b
brevv99 <key.scoup55@gmail.com>
2021-03-26 15:32:55 +0700

    Belajar switch statement

---
+++ src/main/java/Bagian2/TheswitchStatement.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class TheswitchStatement {
+ 
+    //belajar switch case statement
+    public static void main(String[]args)
+    {
+       int x = 10;
+       
+       switch (x)
+       {
+        case  10:
+
+        System.out.println("A");
+
+        break;
+        
+        case 20:
+
+        System.out.println("B");
+
+        break; 
+       }
+    }
+    
+}
