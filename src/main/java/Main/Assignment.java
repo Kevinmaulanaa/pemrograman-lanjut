cc0d66cac9220c5fa8420ef5b6ba2007eced905c
brevv99 <key.scoup55@gmail.com>
2021-03-26 14:15:24 +0700

    Belajar Operator Assignment

---
+++ src/main/java/Main/Assignment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author User
+ */
+public class Assignment {
+    //ini adalah materi assignment dari solo learn
+    public static void main(String[]args){
+        
+    int x = 25; int y;
+    
+    y = x - 12;
+
+System.out.println(y);
+}
+}
