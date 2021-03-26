daf7343842f1ed87907691fbfe882dd3708b1109
brevv99 <key.scoup55@gmail.com>
2021-03-26 15:10:05 +0700

    Belajar materi logika statement

---
+++ src/main/java/Bagian2/Logical.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class Logical {
+    public static void main(String[]args)
+    {
+        int age = 23;
+
+    int money = 4000;
+
+    if (age > 21 && money > 500) {
+
+   System.out.println("Welcome");
+
+    }
+    }
+}
