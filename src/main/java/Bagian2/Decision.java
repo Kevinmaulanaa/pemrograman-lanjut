25cf6111d4701709e8301695db772d12e46158d1
brevv99 <key.scoup55@gmail.com>
2021-03-26 14:32:07 +0700

    Lanjutan Belajar Sololearn

---
+++ src/main/java/Bagian2/Decision.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class Decision {
+    //Decision Making
+    //Fill in the blanks to print "Yeah".
+    
+    public static void main(String[]args)
+    {
+        int x = 5;
+        
+        if ( x == 5 ) {
+        
+            System.out.println("Yeah");
+
+    }
+    }
+}
---
+++ src/main/java/Main/TimeConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+
+public class TimeConverter {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int days = scanner.nextInt();
+		
+		//your code goes here
+		int b = 24;
+  		int c = 60;
+  		int d = 60;  
+  		//int sum1 = a*b; 
+		int sum1 = days * b;               
+  		int sum2 = sum1*c;
+  		int sum3 = sum2*d;
+
+ 
+		System.out.println(sum3);  
+		
+	}
+}
