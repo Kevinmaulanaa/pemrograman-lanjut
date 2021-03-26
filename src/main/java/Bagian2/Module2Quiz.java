5ad19321f380c4eeb593324570de20b091f82642
brevv99 <key.scoup55@gmail.com>
2021-03-26 16:50:15 +0700

    Kuis Bagian 2

---
+++ src/main/java/Bagian2/Module2Quiz.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class Module2Quiz {
+    public static void main(String[]args)
+    {
+            
+        //kuis bagian 1
+        
+        int x = 1;
+
+        while (x <= 7 ) {
+
+        System.out.println("in a loop");
+        
+        x++;
+
+        }
+        
+        //kuis bagian 2 You rock!
+        
+        int a = 144;
+
+        int b = 33;
+        
+        if (a > 15 && b <= 72) {
+            
+        System.out.println("You rock!");
+    }
+        
+        //kuis bagian 3 in a loop
+        
+        for(int d = 0; d < 5; d++)
+        {
+            System.out.println("In a loop");
+        }
+        
+}
+}
+
