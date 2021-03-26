f09c4dbe89f9ff9b18a85d5696394dcf1e21fa02
brevv99 <key.scoup55@gmail.com>
2021-03-26 16:59:56 +0700

    Belajar inisialisasi array

---
+++ src/main/java/Bagian3/InitializingArrays.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian3;
+
+/**
+ *
+ * @author User
+ */
+public class InitializingArrays {
+    public static void main(String[]args)
+    {
+        int tmp[ ] = {5, 8, 9, 3};
+        System.out.println(tmp[2]);
+    }
+}
