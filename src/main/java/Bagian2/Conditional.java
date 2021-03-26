56b25a81c6bbe620941839eac01b40c5b4ec823c
brevv99 <key.scoup55@gmail.com>
2021-03-26 14:52:21 +0700

    Menambahkan Materi Coditional

---
+++ src/main/java/Bagian2/Conditional.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian2;
+
+/**
+ *
+ * @author User
+ */
+public class Conditional {
+    //belajar materi conditional statement 
+    
+    public static void main(String[]args){
+    
+    int x = 10; int y = 5;
+    
+    if(x > y)
+    {
+        System.out.println(x);
+    }else{
+        System.out.println(y);
+    }
+}
+}
