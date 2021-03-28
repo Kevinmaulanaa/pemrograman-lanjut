9297c9005acfbb5c3cd3dfdfcf562ef0a3f90c75
brevv99 <key.scoup55@gmail.com>
2021-03-28 14:31:26 +0700

    Belajar Array Multidimensi

---
+++ src/main/java/Bagian3/Multidimensional.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian3;
+
+/**
+ *
+ * @author User
+ */
+public class Multidimensional {
+
+    public static void main(String[]args)
+    {
+
+        String array[ ][ ] =  { {"Hey", "John", "Bye"},{"John", "Johnson", "Hello"} }; 
+		
+		System.out.println(array[1][1]);
+    }
+}
