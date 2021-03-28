7e19a3e6b18b3d629f7cd20eeb11602730189709
brevv99 <key.scoup55@gmail.com>
2021-03-28 14:27:36 +0700

    Belajar Enhanced for Loop

---
+++ src/main/java/Bagian3/EnhancedforLoop.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Bagian3;
+
+/**
+ *
+ * @author User
+ */
+public class EnhancedforLoop {
+
+    public static void main(String[]args)
+    {
+
+        int total = 0;
+
+        for(int x : arr ) {
+
+        total += x;
+
+    }
+    }
+}
